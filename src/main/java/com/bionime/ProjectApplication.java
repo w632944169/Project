dec07f99194dce56a748e19d0712b9425b3eb1af
Ben.Guo <632944169@qq.com>
2020-12-02 13:22:47 +0800

    no message

--- src/main/java/com/bionime/ProjectApplication.java
+++ src/main/java/com/bionime/ProjectApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @SpringBootApplication
-public class ClientApplication {
+public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
